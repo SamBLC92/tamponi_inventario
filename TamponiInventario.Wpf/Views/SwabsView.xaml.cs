57a4c9356a56b841f2cc61186a2ae8b97e46bb90
SamBLC92 <96847875+SamBLC92@users.noreply.github.com>
2026-01-25 14:09:12 +0100

    Add label barcode generation service

--- TamponiInventario.Wpf/Views/SwabsView.xaml.cs
+++ TamponiInventario.Wpf/Views/SwabsView.xaml.cs
@@ -1,11 +1,24 @@
+using System.Windows;
 using System.Windows.Controls;
+using TamponiInventario.Wpf.Data;
+using TamponiInventario.Wpf.Services;
 
 namespace TamponiInventario.Wpf.Views;
 
 public partial class SwabsView : UserControl
 {
+    private readonly LabelService _labelService = new();
+
     public SwabsView()
     {
         InitializeComponent();
+    }
+
+    private void OpenLabel_Click(object sender, RoutedEventArgs e)
+    {
+        if (sender is Button { DataContext: SwabOverview swab })
+        {
+            _labelService.OpenLabel(swab.Sku);
+        }
     }
 }
