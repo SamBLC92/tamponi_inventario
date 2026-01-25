e18558261f3d189142ffb40558e9c226b37b43a2
SamBLC92 <96847875+SamBLC92@users.noreply.github.com>
2026-01-25 14:04:01 +0100

    Add WPF views for swabs scan history and admin

---
+++ TamponiInventario.Wpf/Views/Admin/AdminView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace TamponiInventario.Wpf.Views.Admin;
+
+public partial class AdminView : UserControl
+{
+    public AdminView()
+    {
+        InitializeComponent();
+    }
+}
